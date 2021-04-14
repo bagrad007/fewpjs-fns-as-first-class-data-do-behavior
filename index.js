fe47c4fe7287dd4b2df9685d44e74d6dabb0fabb
Luke Gill <bagrad007@gmail.com>
2021-04-14 14:49:18 -0400

    commit tree 3451cc770766c3d4c8a550bb0d54f2731781158b

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let splitTime = time.split(":")
+  let newTime = parseInt(splitTime)
+
+  if (newTime <= 12) {
+    return "Good Morning"
+  } else if (newTime > 12 && newTime < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string) {
+  let h1 = document.getElementById("greeting")
+  h1.innerText = string
+}
